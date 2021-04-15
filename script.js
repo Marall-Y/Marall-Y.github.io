ce0f7d1c4077cdc4fb858e16c9c35faff48d40e6
Maral <maralyoosefi@gmail.com>
2021-04-15 21:06:18 +0430

    Edit1

--- script.js
+++ script.js
@@ -1,48 +1,29 @@
-"use strict";
-
-/*const panelofContainer = document.querySelectorAll(".panel");
-const forwardBtn = document.querySelector("#btn-forward");
-const backwardBtn = document.querySelector("#btn-back");
-
-let currenActive = 1;
-
-function update() {
-  panelofContainer.forEach((panel, idx) => {
-    if (idx < currenActive) {
-      panel.classList.add("active");
-    } else {
-      panel.classList.remove("active");
-    }
+$(document).ready(function ($) {
+  "use strict";
+  $("#customers-testimonials").owlCarousel({
+    loop: true,
+    center: true,
+    items: 3,
+    margin: 30,
+    autoplay: true,
+    dots: true,
+    nav: true,
+    autoplayTimeout: 8500,
+    smartSpeed: 450,
+    navText: [
+      '<i class="fa fa-angle-left"></i>',
+      '<i class="fa fa-angle-right"></i>',
+    ],
+    responsive: {
+      0: {
+        items: 1,
+      },
+      768: {
+        items: 2,
+      },
+      1170: {
+        items: 3,
+      },
+    },
   });
-}
-
-forwardBtn.addEventListener("click", function () {
-  currenActive++;
-
-  if (currenActive >= panelofContainer.length) {
-    currenActive = panelofContainer.length;
-  }
-  console.log(currenActive);
-  update();
 });
-
-backwardBtn.addEventListener("click", function () {
-  currenActive--;
-
-  if (currenActive < 1) {
-    currenActive = 1;
-  }
-  console.log(currenActive);
-  update();
-});
-*/
-
-const slides = document.querySelectorAll(".panel");
-let currentSlide = 0;
-let slideInterval = setInterval(nextSlide, 2000);
-
-function nextSlide() {
-  slides[currentSlide].classList.remove("active");
-  currentSlide = (currentSlide + 1) % slides.length;
-  slides[currentSlide].className = "panel active";
-}
